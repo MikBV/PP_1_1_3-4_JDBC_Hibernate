912d141f7519e9b454363a56d6de316f799e4729
MikBV <bmv00791@gmail.com>
2024-10-16 05:30:49 +0800

    Settings for Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.*;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/kata_1_1_3_db";
+    private static final String ROOT_NAME = "root";
+    private static final String ROOT_PASSWORD = "";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL,ROOT_NAME,ROOT_PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
 }
